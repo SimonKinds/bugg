b7ec3c845f750495807e2b33e02a7b602e0f5dcb
Simon Kindström <simon.kindstrom@gmail.com>
2018-10-21 21:13:47 +0200

    Add testing framework to client

---
+++ client/pages/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render, cleanup} from 'react-testing-library'
+
+import App from './index';
+
+afterEach(cleanup);
+
+it('Can render without crashing', () => {
+    render(<App />);
+});
