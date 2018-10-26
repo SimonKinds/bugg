de8f63c324f280d1dac21efb0a2d8ce5128c1cd1
Simon Kindström <simon.kindstrom@gmail.com>
2018-10-26 21:20:19 +0200

    Enable CSS includes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,26 @@
 // @flow
 
-export default () => "Next.js setup";
+import React from "react";
+
+import "./styles.css";
+
+function IndexPage() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Next.js page!</p>
+      </main>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Bugg</h1>
+    </header>
+  );
+}
+
+export default IndexPage;
