9d6d808f75b8a46300485cbf8efb03d82c2c21ac
Simon Kindström <simon.kindstrom@gmail.com>
2018-11-10 10:13:56 +0100

    Extract CouplePicker to separate file

--- client/components/NoteTaker/NoteTaker.js
+++ client/components/NoteTaker/NoteTaker.js
@@ -4,6 +4,8 @@ import React from "react";
 import type { Node as ReactNode } from "react";
 import Modal from "react-modal";
 
+import CouplePicker from "./CouplePicker";
+
 import "./styles.css";
 
 type CriterionViewModel = {
@@ -222,54 +224,6 @@ class NoteTakingForm extends React.Component<
       </form>
     );
   }
-}
-
-type CouplePickerProps = {
-  coupleIdsForHumans: Array<string>,
-  selectedCoupleIdForHuman: string,
-  onClick: (coupleIdForHumans: string) => void
-};
-function CouplePicker({
-  coupleIdsForHumans,
-  selectedCoupleIdForHuman,
-  onClick
-}: CouplePickerProps) {
-  return (
-    <div className="couple-picker-container">
-      {coupleIdsForHumans.map(coupleIdForHuman => (
-        <CouplePickerButton
-          key={coupleIdForHuman}
-          coupleIdForHumans={coupleIdForHuman}
-          onClick={onClick}
-          selected={coupleIdForHuman === selectedCoupleIdForHuman}
-        />
-      ))}
-    </div>
-  );
-}
-
-type CouplePickerButtonProps = {
-  coupleIdForHumans: string,
-  selected: boolean,
-  onClick: (coupleIdForHumans: string) => void
-};
-function CouplePickerButton({
-  coupleIdForHumans,
-  selected,
-  onClick
-}: CouplePickerButtonProps) {
-  const extendedClassName = selected ? "couple-picker--selected" : "";
-  const className = "couple-picker " + extendedClassName;
-
-  return (
-    <button
-      type="button"
-      className={className}
-      onClick={() => onClick(coupleIdForHumans)}
-    >
-      {coupleIdForHumans}
-    </button>
-  );
 }
 
 type NoteTakerColumnProps = {
