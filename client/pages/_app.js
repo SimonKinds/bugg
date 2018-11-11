852838cfef53b0a7eb8623852c88fb2abf1101d9
Simon Kindström <simon.kindstrom@gmail.com>
2018-11-11 09:40:14 +0100

    Extract html and body styling to styled-components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,22 @@
 import React from "react";
 import App, { Container } from "next/app";
 import Modal from "react-modal";
+import { createGlobalStyle } from "styled-components";
 
 Modal.setAppElement("#__next");
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    --max-content-width: 768px;
+
+    margin: 0;
+    background-color: #fafafa;
+  }
+`;
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -20,6 +34,7 @@ export default class MyApp extends App {
 
     return (
       <Container>
+        <GlobalStyle />
         <Component {...pageProps} />
       </Container>
     );
