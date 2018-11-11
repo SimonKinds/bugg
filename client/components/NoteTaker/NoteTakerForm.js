3e313f888bd2f8389dc54cbb646269f69ae64c07
Simon Kindström <simon.kindstrom@gmail.com>
2018-11-11 09:00:55 +0100

    Style NoteTakerForm with styled-components

--- client/components/NoteTaker/NoteTakerForm.js
+++ client/components/NoteTaker/NoteTakerForm.js
@@ -1,6 +1,97 @@
 // @flow
 import React, { type Node as ReactNode } from "react";
 import Modal from "react-modal";
+import styled from "styled-components";
+
+const StyledNoteTakingArea = styled.div`
+  display: flex;
+  margin-bottom: 2rem;
+`;
+
+const StyledSubmitButton = styled.button`
+  float: right;
+
+  height: 5rem;
+  width: 12rem;
+  margin: 0;
+
+  outline: none;
+
+  border: none;
+  border-radius: 0.8rem;
+
+  text-decoration: none;
+  background: #569ab8;
+  color: #fefefe;
+  font-family: sans-serif;
+  font-size: 2rem;
+  cursor: pointer;
+  text-align: center;
+
+  :active {
+    transform: scale(0.99);
+  }
+
+  :hover,
+  :focus {
+    background: #4b8ba7;
+  }
+`;
+
+const StyledCancelButton = styled.button`
+  height: 5rem;
+  width: 12rem;
+  margin: 0;
+
+  outline: none;
+
+  border: #ccc 2px solid;
+  border-radius: 0.8rem;
+
+  text-decoration: none;
+  background: #fefefe;
+  color: #222;
+  font-family: sans-serif;
+  font-size: 2rem;
+  cursor: pointer;
+  text-align: center;
+
+  margin-right: 1rem;
+
+  :active {
+    transform: scale(0.99);
+  }
+
+  :hover,
+  :focus {
+    background: #cccccc50;
+  }
+`;
+
+const StyledButtonRow = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
+`;
+
+const StyledModalHeader = styled.h1`
+  margin-top: 5rem;
+  text-align: center;
+  font-size: 3rem;
+`;
+
+// $FlowFixMe
+const StyledModal = styled(Modal)`
+  border: #ccc 2px solid;
+  border-radius: 0.5rem;
+  width: 45rem;
+  height: 25rem;
+  background: #fefefe;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) !important;
+`;
 
 type CriterionViewModel = {
   criterionName: string,
@@ -75,46 +166,42 @@ class NoteTakerForm extends React.Component<
     );
 
     return (
-      <form>
-        <Modal
+      <form
+        onSubmit={(e: SyntheticEvent<HTMLInputElement>) => {
+          e.preventDefault();
+          this.setState({ showConfirmationModal: true });
+        }}
+      >
+        <StyledModal
           isOpen={this.state.showConfirmationModal}
           onRequestClose={() => this.setState({ showConfirmationModal: false })}
-          className="modal"
         >
-          <h1 className="modal__header">Submit notes</h1>
-          <div className="modal__button-row">
-            <button
+          <StyledModalHeader>Submit notes</StyledModalHeader>
+          <StyledButtonRow>
+            <StyledCancelButton
               type="button"
-              className="modal__cancel-button"
               onClick={() => this.setState({ showConfirmationModal: false })}
             >
               Cancel
-            </button>
-            <button
-              type="submit"
-              className="note-taking-submit-button"
+            </StyledCancelButton>
+            <StyledSubmitButton
+              type="button"
               onClick={() => {
                 this.setState({ showConfirmationModal: false });
                 this.props.onSubmit(this.state.selectedValues);
               }}
             >
               Submit
-            </button>
-          </div>
-        </Modal>
-        <div className="note-taking-area">
+            </StyledSubmitButton>
+          </StyledButtonRow>
+        </StyledModal>
+        <StyledNoteTakingArea>
           {this.props.children({
             childProps
           })}
-        </div>
+        </StyledNoteTakingArea>
 
-        <button
-          type="button"
-          className="note-taking-submit-button"
-          onClick={() => this.setState({ showConfirmationModal: true })}
-        >
-          Submit
-        </button>
+        <StyledSubmitButton type="submit">Submit</StyledSubmitButton>
       </form>
     );
   }
