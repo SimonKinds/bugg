55232d8ceb5fd3f13401349ee8185f504ca7addb
Simon Kindström <simon.kindstrom@gmail.com>
2018-11-20 20:21:02 +0100

    Test for CouplePicker

---
+++ client/components/NoteTaker/CouplePicker.test.js
@@ -0,0 +1,32 @@
+// @flow
+
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+
+import CouplePicker from "./CouplePicker";
+
+afterEach(cleanup);
+
+it("Can render without crashing", () => {
+  render(
+    <CouplePicker
+      coupleIdsForHumans={[]}
+      selectedIndex={0}
+      onClick={() => {}}
+    />
+  );
+});
+
+it("onClick is called when clicking on couple", done => {
+  const { getByText } = render(
+    <CouplePicker
+      coupleIdsForHumans={["couple-id"]}
+      selectedIndex={0}
+      onClick={() => {
+        done();
+      }}
+    />
+  );
+
+  expect(getByText("couple-id").click());
+});
